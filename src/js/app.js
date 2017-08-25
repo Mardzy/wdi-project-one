c8ea3d2af96085f1f6887d6afb5848ffc0fd78c8
Mardzy <mardlingp@gmail.com>
2017-08-25 12:48:32 +0100

    colors

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-console.log('oi');
+$(() => {
+  const $play = $('.play');
+  let playingGame = false;
+  let playText = $($play).text('Start Game');
+
+  function playGame (){
+    if(!playingGame){
+      console.log('click');
+
+      return playText;
+    } else {
+      playingGame = true;
+      playText = $($play).text('Start Game');
+      return playText;
+    }
+  }
+  $($play.on('click',playGame));
+
+  console.log(playGame);
+});
