f590605e1c70003dc6ead6ae90b782ac71dedd69
Mardzy <mardlingp@gmail.com>
2017-08-26 09:55:25 +0100

    styling

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
 $(() => {
+
+//variables
+  const $square = $('.square');
   const $play = $('.play');
   let playingGame = false;
   let playText = $($play).text('Start Game');
 
+  // functions
   function playGame (){
     if(!playingGame){
       console.log('click');
@@ -14,7 +18,25 @@ $(() => {
       return playText;
     }
   }
-  $($play.on('click',playGame));
 
+  function clickSquare() {
+    console.log('clicked square');
+  }
+
+  // jquery listeners
+  $play.on('click',playGame);
+
+  $square.on('click', clickSquare);
   console.log(playGame);
-});
+});// last line
+
+
+// const dice ={
+//   one: 1,
+//   two: 2,
+//   three: 3,
+//   four: 4,
+//   five: 5,
+//   six: 6
+//
+// };
