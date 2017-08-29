196e350b3cf3c27bc8db84df352ad7487316507c
Mardzy <mardlingp@gmail.com>
2017-08-29 12:01:17 +0100

    lost

--- src/js/app.js
+++ src/js/app.js
@@ -1,164 +1,4 @@
-//*************update the data model and render**************
-//
-//
-// $(() => {
-//
-//   //variables
-//
-//   //buttons
-//   const $rules = $('#rules');
-//   const $reset = $('#reset');
-//   const $startGame = $('#start-game');
-//   // const $newRound = $('#new-round');
-//   const $rollDice = $('#roll-dice');
-//   // const $hold = $('#hold');
-//
-//
-//   //display elements
-//   const $result = $('#result');
-//   const $gameContainer = $('#game-container');
-//   const $hiddenWrapper = $('#hidden-wrapper');
-//   const $playerContainer = $('#player-container');
-//   const $computerContainer = $('#cpu-container');
-//   const $wins= $('#wins span');
-//   const $losses= $('#losses span');
-//   const wins= 0;
-//   const losses= 0;
-//
-//   //game logic
-//   // const playerDice =[];
-//   // const computerDice =[];
-//
-//   //let playingGame = false;
-//
-//   function createRandomNumbers(length,min,max){
-//     const randomNumbers =[];
-//     for (let i = 0; i<length;i++){
-//       min = Math.ceil(min);
-//       max = Math.floor(max);
-//       const random= Math.floor(Math.random() * (max - min + 1)) + min;
-//       randomNumbers.push(random);
-//     }
-//     return randomNumbers;
-//   }
-//
-//   var model = {
-//     playerHand: createRandomNumbers(5,1,6),
-//     computerHand: createRandomNumbers(5,1,6),
-//     extraRolls:
-//     highlightedIndices: [],
-//     winningHands: []
-//   };
-//
-//   function startGame(){
-//     render(model);
-//   }
-//
-//   function loopHighlighted(){
-//     for(let i =0; i < model.highlightedIndices.length; i++){
-//       const highlightedIndex = model.highlightedIndices[i];
-//       const elem = $playerContainer.find('.square')[highlightedIndex];
-//       $(elem).addClass('highlighted');
-//     }
-//
-//   }
-//
-//   function clickSquare(idx) {
-//     if (model.highlightedIndices.length <3)
-//       model.highlightedIndices.push(idx);
-//     render();
-//   }
-//   function reset(){
-//     $playerContainer.html('');
-//     $computerContainer.html('');
-//     $wins.text(wins);
-//     $losses.text(losses);
-//     $result.html('Welcome to Poker Dice!!!</br>Click Start Game');
-//   }
-//
-//   function rollPlayerDice(){
-//     model.playerHand.forEach((value, idx) => {
-//       const elem = $('<div class="square"></div>');
-//       elem.addClass('die-' + value);
-//       $playerContainer.append(elem);
-//       elem.on('click', () => clickSquare(idx));
-//     });
-//   }
-//
-//   removeHighlightedElement(){
-//     model.highlightedIndices.forEach((value, idx) => {
-//       const removeElem
-//     })
-//   }
-//
-//   rollHighlighted(){
-//     model.playerHand.forEach((value, idx) => {
-//       const elem
-//       const elem = $('<div class="square"></div>');
-//       elem.addClass('die-' + value);
-//       $playerContainer.append(elem);
-//       elem.on('click', () => clickSquare(idx));
-//     });
-//   }
-//
-//   function rollComputerDice(){
-//     model.computerHand.forEach(value => {
-//       const elem = $('<div id="hidden-dice" class="square "></div>');
-//       elem.addClass('die-' + value);
-//       $computerContainer.append(elem);
-//     });
-//   }
-//
-//   // function rollHighlighted(){
-//   //
-//   // }
-//
-//   function render(){
-//     reset();
-//     rollPlayerDice();
-//     rollComputerDice();
-//     loopHighlighted();
-//   }
-//
-//
-//   console.log(createRandomNumbers(5,1,6));
-//
-//
-//
-//   // let playText = $($result).text('Start Game');
-//   // functions
-//   // function playGame (){
-//   //   if(!playingGame){
-//   //     console.log('click');
-//   //     return playText;
-//   //   } else {
-//   //     playingGame = true;
-//   //     playText = $($newRound).text('Start Game');
-//   //     return playText;
-//   //   }
-//   // }
-//
-//
-//
-//   // jquery listeners
-//   function showRules(){
-//     if($gameContainer.css('display')!=='none'){
-//       $gameContainer.css({'display': 'none'});
-//       $hiddenWrapper.css({'display': 'flex'});
-//     } else {
-//       $hiddenWrapper.css({'display': 'none'});
-//       $gameContainer.css({'display': 'flex'});
-//     }
-//
-//   }
-//   //$rolldice.on('click',)
-//   $rules.on('click',showRules);
-//   $reset.on('click',reset);
-//   $startGame.on('click',startGame);
-//   $rollDice.on('click',rollPlayerDice);
-//
-// });// last line inside dom
-//*************update the data model and render**************
+// *************update the data model and render**************
 
 
 $(() => {
@@ -170,15 +10,14 @@ $(() => {
   const $reset = $('#reset');
   const $startGame = $('#start-game');
   // const $newRound = $('#new-round');
-  // const $rollDice = $('#roll-dice');
+  const $rollDice = $('#roll-dice');
   // const $hold = $('#hold');
 
 
   //display elements
-  //const $square = $('.square');
+  const $result = $('#result');
   const $gameContainer = $('#game-container');
   const $hiddenWrapper = $('#hidden-wrapper');
-
   const $playerContainer = $('#player-container');
   const $computerContainer = $('#cpu-container');
   const $wins= $('#wins span');
@@ -186,6 +25,7 @@ $(() => {
   const wins= 0;
   const losses= 0;
 
+  //game logic
   // const playerDice =[];
   // const computerDice =[];
 
@@ -193,7 +33,7 @@ $(() => {
 
   function createRandomNumbers(length,min,max){
     const randomNumbers =[];
-    for (let i = 0; i<length;i){
+    for (let i = 0; i<length;i++){
       min = Math.ceil(min);
       max = Math.floor(max);
       const random= Math.floor(Math.random() * (max - min + 1)) + min;
@@ -205,7 +45,6 @@ $(() => {
   var model = {
     playerHand: createRandomNumbers(5,1,6),
     computerHand: createRandomNumbers(5,1,6),
-
     highlightedIndices: [],
     winningHands: []
   };
@@ -215,7 +54,7 @@ $(() => {
   }
 
   function loopHighlighted(){
-    for(let i =0; i < model.highlightedIndices.length; i){
+    for(let i =0; i < model.highlightedIndices.length; i++){
       const highlightedIndex = model.highlightedIndices[i];
       const elem = $playerContainer.find('.square')[highlightedIndex];
       $(elem).addClass('highlighted');
@@ -233,21 +72,50 @@ $(() => {
     $computerContainer.html('');
     $wins.text(wins);
     $losses.text(losses);
+    $result.html('Welcome to Poker Dice!!!</br>Click Start Game');
   }
-  function render(){
-    reset();
+
+  function rollPlayerDice(){
     model.playerHand.forEach((value, idx) => {
       const elem = $('<div class="square"></div>');
       elem.addClass('die-' + value);
       $playerContainer.append(elem);
       elem.on('click', () => clickSquare(idx));
     });
+  }
+
+  // removeHighlightedElement(){
+  //   model.highlightedIndices.forEach((value, idx) => {
+  //     const removeElem
+  //   })
+  // }
+
+  // rollHighlighted(){
+  //   model.playerHand.forEach((value, idx) => {
+  //     const elem
+  //     const elem = $('<div class="square"></div>');
+  //     elem.addClass('die-' + value);
+  //     $playerContainer.append(elem);
+  //     elem.on('click', () => clickSquare(idx));
+  //   });
+  // }
+
+  function rollComputerDice(){
     model.computerHand.forEach(value => {
-      const elem = $('<div class="square"></div>');
+      const elem = $('<div id="hidden-dice" class="square "></div>');
       elem.addClass('die-' + value);
       $computerContainer.append(elem);
     });
+  }
 
+  // function rollHighlighted(){
+  //
+  // }
+
+  function render(){
+    reset();
+    rollPlayerDice();
+    rollComputerDice();
     loopHighlighted();
   }
 
@@ -259,6 +127,13 @@ $(() => {
   // let playText = $($result).text('Start Game');
   // functions
   // function playGame (){
+  //   if(!playingGame){
+  //     console.log('click');
+  //     return playText;
+  //   } else {
+  //     playingGame = true;
+  //     playText = $($newRound).text('Start Game');
+  //     return playText;
   //   }
   // }
 
@@ -275,9 +150,133 @@ $(() => {
     }
 
   }
-
+  //$rolldice.on('click',)
   $rules.on('click',showRules);
   $reset.on('click',reset);
   $startGame.on('click',startGame);
+  $rollDice.on('click',rollPlayerDice);
 
-});// last
+});// last line inside dom
+// *************update the data model and render**************
+
+
+// $(() => {
+//
+//   //variables
+//
+//   //buttons
+//   const $rules = $('#rules');
+//   const $reset = $('#reset');
+//   const $startGame = $('#start-game');
+//   // const $newRound = $('#new-round');
+//   // const $rollDice = $('#roll-dice');
+//   // const $hold = $('#hold');
+//
+//
+//   //display elements
+//   //const $square = $('.square');
+//   const $gameContainer = $('#game-container');
+//   const $hiddenWrapper = $('#hidden-wrapper');
+//
+//   const $playerContainer = $('#player-container');
+//   const $computerContainer = $('#cpu-container');
+//   const $wins= $('#wins span');
+//   const $losses= $('#losses span');
+//   const wins= 0;
+//   const losses= 0;
+//
+//   // const playerDice =[];
+//   // const computerDice =[];
+//
+//   //let playingGame = false;
+//
+//   function createRandomNumbers(length,min,max){
+//     const randomNumbers =[];
+//     for (let i = 0; i<length;i){
+//       min = Math.ceil(min);
+//       max = Math.floor(max);
+//       const random= Math.floor(Math.random() * (max - min + 1)) + min;
+//       randomNumbers.push(random);
+//     }
+//     return randomNumbers;
+//   }
+//
+//   var model = {
+//     playerHand: createRandomNumbers(5,1,6),
+//     computerHand: createRandomNumbers(5,1,6),
+//
+//     highlightedIndices: [],
+//     winningHands: []
+//   };
+//
+//   function startGame(){
+//     render(model);
+//   }
+//
+//   function loopHighlighted(){
+//     for(let i =0; i < model.highlightedIndices.length; i){
+//       const highlightedIndex = model.highlightedIndices[i];
+//       const elem = $playerContainer.find('.square')[highlightedIndex];
+//       $(elem).addClass('highlighted');
+//     }
+//
+//   }
+//
+//   function clickSquare(idx) {
+//     if (model.highlightedIndices.length <3)
+//       model.highlightedIndices.push(idx);
+//     render();
+//   }
+//   function reset(){
+//     $playerContainer.html('');
+//     $computerContainer.html('');
+//     $wins.text(wins);
+//     $losses.text(losses);
+//   }
+//   function render(){
+//     reset();
+//     model.playerHand.forEach((value, idx) => {
+//       const elem = $('<div class="square"></div>');
+//       elem.addClass('die-' + value);
+//       $playerContainer.append(elem);
+//       elem.on('click', () => clickSquare(idx));
+//     });
+//     model.computerHand.forEach(value => {
+//       const elem = $('<div class="square"></div>');
+//       elem.addClass('die-' + value);
+//       $computerContainer.append(elem);
+//     });
+//
+//     loopHighlighted();
+//   }
+//
+//
+//   console.log(createRandomNumbers(5,1,6));
+//
+//
+//
+//   // let playText = $($result).text('Start Game');
+//   // functions
+//   // function playGame (){
+//   //   }
+//   // }
+//
+//
+//
+//   // jquery listeners
+//   function showRules(){
+//     if($gameContainer.css('display')!=='none'){
+//       $gameContainer.css({'display': 'none'});
+//       $hiddenWrapper.css({'display': 'flex'});
+//     } else {
+//       $hiddenWrapper.css({'display': 'none'});
+//       $gameContainer.css({'display': 'flex'});
+//     }
+//
+//   }
+//
+//   $rules.on('click',showRules);
+//   $reset.on('click',reset);
+//   $startGame.on('click',startGame);
+//
+// });// last
